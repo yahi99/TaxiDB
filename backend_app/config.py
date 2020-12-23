6b87b63ea2b3e4ee916ef4647a1f3e6b3133a741
Vitaliy <elemabor@gmail.com>
2020-12-23 04:06:40 +0300

    update files for Docker

--- backend_app/config.py
+++ backend_app/config.py
@@ -1,6 +1,6 @@
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLACHEMY_DATABASE_URI = 'postgresql://postgres:testpass@localhost/taxi'
+    SQLACHEMY_DATABASE_URI = 'postgresql://docker:password@postgresdb_1:5435/taxi_db'
     HOST_URL = '0.0.0.0'
     HOST_PORT = '5000'
-    white = ['http://localhost:3000','http://localhost:9000']
+    white = ['http://localhost:3000', 'http://localhost:9000']
